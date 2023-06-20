d4d59c75d3f0d90d83b4e8d67960f5c78b41b26f
Sergei Efimov <efimovsergeiv@gmail.com>
2023-06-20 14:38:02 +0300

    add:

--- components/CartBtnSmall.vue
+++ components/CartBtnSmall.vue
@@ -11,7 +11,7 @@
 
     <transition name="fade" mode="out-in">
       <div v-if="productsStore.productInCart(props.product.id)">
-        <button class="">
+        <button @click="productsStore.addProduct('cart', props.product)" class="">
           <div class=" text-sm text-gray-100 rounded-lg bg-blue-600 hover:bg-blue-700 border border-gray-300/50 dark:border-gray-500/50 transition-all duration-1000">
             <div class=" bg-gradient-to-br from-gray-100/20 to-gray-900/40 rounded-lg">
               <p class="text-white text-sm w-36 md:w-24 py-1.5">В корзине</p>
