1c4aa95d1ff3ba6894a9e2a3e6d605d64fe916d1
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-06-27 14:14:23 +0300

    add:

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -20,24 +20,21 @@
 
 <template>
   <div v-if="pages.length > 1" class="">
-    
-    <p class="text-xs">{{ current }}</p>
-    <p class="text-xs">{{ pages }}</p>
 
-    <ul class="flex gap-0.5">
+    <ul class="flex items-center gap-0.5 font-semibold">
       <li>
-        <nuxt-link :to="{ name: 'prods', query: {...route.query, page: 1 }}" class="py-2 px-3 text-gray-500 bg-white rounded-l-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <nuxt-link :to="{ name: 'prods', query: {...route.query, page: 1 }}" class="w-8 h-8 flex items-center justify-center text-gray-500 bg-white rounded-l-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           &lt;
         </nuxt-link>
       </li>
       <li v-for="(page, pk) in pages" :key="pk">
         <nuxt-link :to="{ name: 'prods', query: { ...route.query, page: page }}">
-          <div v-if="Number(current) === Number(page)" class="py-1 px-3 text-gray-800 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-gray-900 dark:border-gray-700 dark:bg-gray-700 dark:text-white">{{ page }}</div>
-          <div v-else class="py-1 px-3 text-gray-500 bg-white rounded-r-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ page }}</div>
+          <div v-if="Number(current) === Number(page)" class="w-8 h-8 flex items-center justify-center text-gray-800 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">{{ page }}</div>
+          <div v-else class="w-8 h-8 flex items-center justify-center text-gray-500 bg-white rounded-r-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ page }}</div>
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link :to="{ name: 'prods', query: {...route.query, page: Math.ceil(props.count/36)}}" class="py-2 px-3 text-gray-500 bg-white rounded-r-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <nuxt-link :to="{ name: 'prods', query: {...route.query, page: Math.ceil(props.count/36)}}" class="w-8 h-8 flex items-center justify-center text-gray-500 bg-white rounded-r-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           &gt;
         </nuxt-link>
       </li>
