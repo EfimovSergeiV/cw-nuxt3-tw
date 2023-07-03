6787f2a8c92b59c3e5b02bd093512b14de894f80
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-07-03 16:40:00 +0300

    add:

--- components/Recommend.vue
+++ components/Recommend.vue
@@ -1,6 +1,8 @@
 <script setup>
-  const props = defineProps(['recommends'])
   const productsStore = useProductsStore()
+  const config = useRuntimeConfig()
+
+  const { data: recommends, pending, error} = await useFetch(`${ config.public.baseURL }c/recommend/`)
 
 </script>
 
@@ -9,7 +11,7 @@
     <div class="my-4 p-2 bg-white rounded-sm border dark:border-gray-700 dark:bg-gray-800 shadow-sm">
       <p class="text-gray-700 dark:text-gray-300 my-4 uppercase">Главный Cварщик рекомендует</p>
       <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        <div v-for="product in props.recommends" :key="product.id" class="">
+        <div v-for="product in recommends" :key="product.id" class="">
           <ProductSmall :product="product" />
         </div>
       </div>      
--- pages/cts.vue
+++ pages/cts.vue
@@ -3,11 +3,6 @@
     titleTemplate: '%s - Категории'
   })
 
-  const config = useRuntimeConfig()
-
-  const { data: cts } = await useFetch(`${ config.public.baseURL }c/ct/`)
-  const { data: recommends } = await useFetch(`${ config.public.baseURL }c/recommend/`)
-
 </script>
 
 
@@ -17,46 +12,8 @@
   <div class="">
     <TopSlider />
     <AppNavbar />
-
-    <div class="mx-auto px-4 lg:max-w-7xl lg:px-8">
-      <div class="bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-700 p-2 ">
-        <div class="columns-1 md:columns-3 lg:columns-4">
-          <div v-for="ct in cts" :key="ct.id" class="break-inside-avoid-column">
-            <div class="p-2">
-              <div class="mb-1">
-                <nuxt-link :to="{ name: 'prods', query: {ct: ct.id}}" class="font-bold text-gray-700 dark:text-gray-300 text-base">{{ ct.name }}</nuxt-link>              
-              </div>
-              <div>
-                <ul>
-                  <li v-for="sct in ct.inserted" :key="sct.id" class="inline-block mr-3 my-1 group relative">
-                    
-                    <nuxt-link v-if="sct.inserted.length === 0" :to="{ name: 'prods', query: {ct: sct.id}}" class="text-gray-700 text-sm hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">{{ sct.name }}</nuxt-link>
-                    <button v-else class="text-gray-700 text-sm hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">{{ sct.name }}</button>
-
-                    <div class="absolute invisible group-hover:opacity-100 group-hover:visible right-0 z-40" v-if="sct.inserted.length > 0">
-                      
-                      <div class="bg-white border-gray-200 border rounded-sm dark:border-gray-700 dark:bg-gray-800 p-4">
-                        <nuxt-link :to="{ name: 'prods', query: {ct: sct.id}}" class="font-bold">{{ sct.name }}</nuxt-link>
-                        <div class="my-2 md:columns-2">
-                          <div v-for="ssct in sct.inserted" :key="ssct.id" class="px-1">
-                            <nuxt-link :to="{ name: 'prods', query: {ct: ssct.id}}" class="text-sm text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">{{ ssct.name }}</nuxt-link>
-                          </div>                                 
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-
-              </div>              
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    <Recommend :recommends="recommends" />
-
+    <Categories />
+    <Recommend />
     <CorouselBrands />
   </div>
 </template>
