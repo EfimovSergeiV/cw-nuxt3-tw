9ee55af85070cf560f4b8766f40022d226124b53
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-07-17 12:13:42 +0300

    add:

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -3,7 +3,7 @@
 
 
   const config = useRuntimeConfig()
-  // const { data: products } = await useFetch(`${ config.public.baseURL }c/neues/`)
+  // const { data: products } = await useFetch(`${ config.public.baseURL }c/neues/`) /// for debug styles
 
   const search = ref('')
   const products = ref([])
@@ -37,17 +37,16 @@
 
         
         <div class="w-full">
-          <div class="relative w-full">
+          <div class="relative w-full group">
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
               <p class="mdi mdi-24px mdi-magnify"></p>
             </div>
             <input v-model="search" type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-14 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 ring-0 dark:focus:ring-gray-600 dark:focus:border-gray-600" placeholder="Поиск по товарам">
           
-            <div class="absolute z-30 w-full hid den">
-              <div class="dark:bg-gray-700 border dark:border-gray-600 rounded-sm my-1 min-h-[80px]">
+            <div class="absolute z-30 w-full invisible group-hover:visible ease-in-out transition-opacity duration-100 opacity-0 group-hover:opacity-100">
+              <div class="bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-sm my-1 min-h-[80px]">
                 
                 <div class="px-2 h-96 overflow-y-auto my-2">
-                  <p class="text-xs">{{ search }}</p>
 
                   <div v-if="search.length > 3 && products.length === 0" class="">
                     <p class="">Ничего не найдено</p>
@@ -57,22 +56,22 @@
                     <p class="">Введите запрос</p>
                   </div>
 
-                  <div class="px-2 py-2 my-1 bg-gray-800/80 border border-gray-500 rounded-md" v-for="product in products" :key="product.id">
-                    <nuxt-link :to="{ name: 'product-id', params: { id: product.id }}" class="">
-                      <div class="flex gap-4">
-                        <div class="">
-                          <img class="bg-white w-20 p-1 rounded-md" :src="product.preview_image" />
+                  <transition-group name="fade">
+                    <div class="px-2 py-2 my-1 bg-gray-100 dark:bg-gray-800/80 border border-gray-200 hover:border-gray-300 dark:border-gray-500 hover:dark:border-gray-400 rounded-md transition-all" v-for="product in products" :key="product.id">
+                      <nuxt-link :to="{ name: 'product-id', params: { id: product.id }}" class="">
+                        <div class="flex gap-4">
+                          <div class="">
+                            <img class="bg-white w-20 p-1 rounded-md" :src="product.preview_image" />
+                          </div>
+                          <div class="">
+                            <p class="">{{ product.name }}</p>
+                            <p v-if="product.only_price > 0" class="">{{ product.only_price.toLocaleString() }} <span class="text-xs">руб.</span></p>
+                            <p v-else class="text-xs">Стоимость по запросу</p>
+                          </div>
                         </div>
-                        <div class="">
-                          <p class="">{{ product.name }}</p>
-                          <p class="">{{ product.only_price.toLocaleString() }} <span class="text-xs">руб.</span></p>
-                        </div>
-                      </div>
-                    </nuxt-link>
-                  </div>
-
-
-
+                      </nuxt-link>
+                    </div>
+                  </transition-group>
 
                 </div>
                 
