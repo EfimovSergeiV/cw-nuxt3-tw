04be5b83d3f2face469080ee8c4874eee7fb25c6
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-08-22 14:27:40 +0300

    add:

--- pages/login.vue
+++ pages/login.vue
@@ -81,33 +81,18 @@ definePageMeta({
             </div>
 
           </div>
-
         </div>
-
-
-
-
-
       </div>
-
-
-      <div></div>
     </div>
 
 
 
 
-    <h1>Login Page</h1>
+    <h1 class="mt-8">Login Page</h1>
     <pre>Status: {{ status }}</pre>
     <pre>Data: {{ data || 'no session data present, are you logged in?' }}</pre>
     <pre>Last refreshed at: {{ lastRefreshedAt || 'no refresh happened' }}</pre>
     <pre>JWT token: {{ token || 'no token present, are you logged in?' }}</pre>
-    <form @submit.prevent="signIn({ username, password })">
-      <input v-model="username" type="text" placeholder="Username">
-      <input v-model="password" type="password" placeholder="Password">
-      <button type="submit">
-        sign in
-      </button>
-    </form>
+
   </div>
 </template>
