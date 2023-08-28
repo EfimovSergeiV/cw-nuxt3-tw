23e1346ee2ba63262bb358aa25f3789b458e082d
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-08-28 15:49:37 +0300

    add:

--- components/SwiperControls.vue
+++ components/SwiperControls.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-	<div class="flex gap-4 items-center justify-end">
-		<button @click="swiper.slideNext()" class="mdi mdi-arrow-left-bold text-gray-100 mdi-24px bg-blue-500/70 hover:bg-blue-500 rounded-full w-10 h-10 duration-500 transition-all"></button>
-		<button @click="swiper.slidePrev()" class="mdi mdi-arrow-right-bold text-gray-100 mdi-24px bg-blue-500/70 hover:bg-blue-500 rounded-full w-10 h-10 duration-500 transition-all"></button>		
+	<div class="flex items-center justify-end bg-blue-700/60 hover:bg-blue-700/90 duration-500 transition-all rounded-lg">
+		<button @click="swiper.slideNext()" class="mdi mdi-arrow-left-bold text-gray-100 mdi-18px w-8 h-8"></button>
+		<button @click="swiper.slidePrev()" class="mdi mdi-arrow-right-bold text-gray-100 mdi-18px w-8 h-8 duration-500 transition-all"></button>		
 	</div>
 
 </template>
