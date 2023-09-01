c435cc6d3432ba519e3394489b999f966b4d6e55
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-09-01 09:55:04 +0300

    add:

--- components/WriteUsModal.vue
+++ components/WriteUsModal.vue
@@ -2,12 +2,39 @@
   const config = useRuntimeConfig()
   // const router = useRouter()
 
-  const productsStore = useProductsStore()
+  const notificationsStore = useNotificationsStore()
   const shopStore = useShopStore()
   const clientStore = useClientStore()
-  // const notificationsStore = useNotificationsStore()
 
-  // const props = defineProps(['product'])
+  const errorMsg = ref(null)
+  const messText = ref(null)
+
+  const sendMessage = async () => {
+    if ( (clientStore.client.city) && (clientStore.client.contact) && (messText.value) ) {
+      const { data: response } = await useFetch(`${ config.public.baseURL }u/feedback/`, {
+        method: 'POST',
+        body: {
+          city: clientStore.client.city,
+          person: clientStore.client.person,
+          contact: clientStore.client.contact,
+          theme: 'order',
+          text: messText.value,
+        }
+        
+      });
+
+      notificationsStore.pushToast({ id: 1, type: 'success', text: 'Ваше сообщение отправлено!' })
+      shopStore.showWriteUsModal()
+
+
+      // order.value = await response.value
+      // productsStore.clearCartProducts()
+      // clientStore.saveClientData(clientData)
+
+    } else {
+      errorMsg.value = 'Ошибка: Для отправки сообщения заполните все поля.'
+    }
+  }
 
 
 </script>
@@ -53,14 +80,18 @@
                       <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                         <p class="mdi mdi-email dark:text-gray-300"></p>
                       </div>
-                      <input v-model="clientStore.client.email" type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@domen.com">
+                      <input v-model="clientStore.client.contact" type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@domen.com">
                     </div>
                   </div>
                 </div>
 
                 <div class="">
                   <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Комментарий к заказу (необязательно)</label>
-                  <textarea v-model="clientStore.client.comment" id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Напишите что-нибудь..."></textarea>
+                  <textarea v-model="messText" id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Напишите что-нибудь..."></textarea>
+                </div>
+
+                <div class="flex items-center justify-end h-6">
+                  <p class="text-red-600 dark:text-red-500 text-sm">{{ errorMsg }}</p>
                 </div>
               </div>
 
@@ -68,7 +99,7 @@
               <div class="grid grid-cols-1 items-end justify-between px-6 py-2 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
                 <div class="flex justify-center gap-2 w-full md:justify-end">             
                   
-                  <button @click="productsStore.showCartModal">
+                  <button @click="sendMessage">
                     <div class=" text-sm text-gray-100 rounded-lg bg-blue-600 hover:bg-blue-700 border border-gray-300/50 dark:border-gray-500/50 transition-all duration-1000">
                       <div class=" bg-gradient-to-br from-gray-100/20 to-gray-900/40 rounded-lg">
                         <p class="px-5 py-2.5">Отправить сообщение</p>
