fc16b52dbc8ea12e44749c321cd18ee2399b212c
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-09-01 15:00:25 +0300

    add:

--- pages/lk/[name].vue
+++ pages/lk/[name].vue
@@ -9,25 +9,25 @@
     <AppNavbar />
 
     <div class="container mx-auto max-w-6xl px-4 lg:px-8">
+      
+      <div class=" min-h-[50vh]">
 
+        <div v-if="status === 'authenticated'" class="">
+          <pre>Status: {{ status }}</pre>
+          <pre>Data: {{ data || 'no session data present, are you logged in?' }}</pre>
+          <pre>Last refreshed at: {{ lastRefreshedAt || 'no refresh happened' }}</pre>
+          <pre>JWT token: {{ token || 'no token present, are you logged in?' }}</pre>
+        </div>
 
-      <h1 class="mt-8">Login Page</h1>
-      <pre>Status: {{ status }}</pre>
-      <pre>Data: {{ data || 'no session data present, are you logged in?' }}</pre>
-      <pre>Last refreshed at: {{ lastRefreshedAt || 'no refresh happened' }}</pre>
-      <pre>JWT token: {{ token || 'no token present, are you logged in?' }}</pre>
+        <div class="py-4">
+          <p>Текущие заказы</p>
+          <p>История заказов</p>
+          <p></p>
+          <p></p>
+        </div>
 
-
-      <div class="py-4">
-
-        <p>Текущие заказы</p>
-        <p>История заказов</p>
-        <p></p>
-        <p></p>
       </div>
 
-
     </div>
-    
   </div>
 </template>
